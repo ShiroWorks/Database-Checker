eb0b4c7b46b8f70f2805b467c40f22cbd84ebeb0
Szabo Vilmos <vili8899s@gmail.com>
2019-02-11 23:56:40 +0100

    done

---
+++ users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
